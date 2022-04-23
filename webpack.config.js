ee01824cc5a3c4cee8f01db60c2ea02f5ec293be
Kenny <kennygbeminiyi@gmail.com>
2022-04-23 06:49:00 +0100

    server setup

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,15 @@ var path = require("path");
 module.exports = {
   entry: "./src/index.js",
   output: {
-    path: path.join(__dirname, "dist", "assets"),
+    path: path.join(__dirname, "public"),
     filename: "bundle.js",
   },
+  // devserver: {
+  //   contentBase: path.resolve(__dirname, 'dist'),
+  //   open: true,
+  //   clientLogLevel: 'silent',
+  //   port: 5000
+  // },
   module: {
     rules: [{ test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }],
   },
