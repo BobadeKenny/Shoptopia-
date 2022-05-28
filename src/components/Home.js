8c94b4fbcc5deefba65c6917eef1e2988724bfeb
Kenny <kennygbeminiyi@gmail.com>
2022-05-28 06:48:45 +0100

    fixed usestate

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,24 +1,26 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
-function requestCategories() {
-	const [categories, setCategories] = useState();
-	useEffect(() => {
-		fetch(`https://fakestoreapi.com/products/categories`)
-			.then((res) => res.json())
-			.then((json) => setCategories)
-			.catch(console.error);
-	}, [])
-	return (
-		categories.map(category => <Link to={`/category/${category}`}>${category}</Link>)
-	)
-}
+
 export default function Home()
 {
+	const [categories, setCategories] = useState([]);
+	function requestCategories() {
+		fetch(`https://fakestoreapi.com/products/categories`)
+		.then((res) => res.json())
+		.then((json) => setCategories(json))
+		.catch(console.error);
+	}
+	useEffect(() => {
+		requestCategories();
+	}, []);
 	return (
 		<div>
 			<h1>Header</h1>
-			<requestCategories/>
+			<h1>Header</h1>
+			<div>
+				{categories.map(category => <Link to={`/category/${category}`}>{category}</Link>)}
+			</div>
 			<Link to='c'>Contact</Link>
 		</div>
 	)
